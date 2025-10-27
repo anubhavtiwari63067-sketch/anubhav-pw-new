619b6bb3d510f242d4f82e2f52f54cda8b01b15d
anubhavtiwari63067 <anubhavtiwari63067@gmail.com>
2025-10-27 11:33:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// Array banate hai:
+// let fruit = [apple,banana,originAgentCluster,papaya];
+
+// kisi element ko access krne ke liye:
+// console.log(fruit[1]);
+
+// Arrays me loop lagana:
+// for(let i = 0; i<fruits.length; i++);{
+//     console.log(fruits[1]);
+// }
+// let marks =[89,67,88,93,55]
+// console.log(marks[1]);
+
+// arrays me adding kaise krte hai:
+// let number=[24,46,62,33,89]
+// let sum = number.reduce((total, num) <= total + num , 0) ;
+// console.log(sum);
+
+//if else condition se checkout krna ki even h ya odd;
+
+console.log("even");
+
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+}
+for (let i = 4; i <= 10; i += 2) {
+    console.log(i);
+}
+for (let i = 6; i <= 10; i += 2) {
+    console.log(i);
+}
+for (let i = 8; i <= 10; i += 2) {
+    console.log(i);
+}
+for (let i = 10; i <= 10; i += 2) {
+    console.log(i);
+}
+for (let i = 12; i <= 10; i += 2) {
+    console.log(i);
+}
+console.log("loop is end !");
+
+console.log("odd");
+for(let i = 1; i <= 10; i += 2){
+    console.log(i);
+}
+for(let i = 3; i <= 10; i += 2){
+    console.log(i);
+}
+for(let i = 5; i <= 10; i += 2){
+    console.log(i);
+}
+for(let i = 7; i <= 10; i += 2){
+    console.log(i);
+} 
+for(let i = 9; i <= 10; i += 2){
+    console.log(i);
+}
+for(let i = 11; i <= 10; i += 2){
+    console.log(i);
+}
+// else("11 is not less then 10"){
+//     console.log("loop is end!");
+// }
+console.log(" the loop is end")
+        
+        
+
+
